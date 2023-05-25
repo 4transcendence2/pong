8134166190b74200f365ee37476a8aedcc56f275
sumnii <sumsong@student.42seoul.kr>
2023-05-25 21:15:59 +0900

    [FEAT] compress avatar img func

--- app/frontend/src/src/modal/AvatarUploadModal.tsx
+++ app/frontend/src/src/modal/AvatarUploadModal.tsx
@@ -2,6 +2,7 @@ import { postAvatar } from "api/user";
 import * as S from "./layout/style";
 import React, { useRef, useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import imageCompression from "browser-image-compression";
 
 type modalProps = {
   username: string | undefined;
@@ -11,7 +12,7 @@ type modalProps = {
 
 function AvatarUploadModal(props: modalProps) {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [fileList, setFileList] = useState<FileList>();
+  const [uploadedImg, setUploadedImg] = useState<File>();
   const [tmpImg, setTmpImg] = useState<string>("");
   const [noti, setNoti] = useState("");
   const queryCli = useQueryClient();
@@ -26,17 +27,32 @@ function AvatarUploadModal(props: modalProps) {
     clearTimeout(id);
   };
 
-  const check = () => {
+  const compressImg = async (img: File) => {
+    const option = {
+      maxSizeMb: 0.02,
+      maxWidthOrHeight: 200,
+    };
+
+    const compressedImg = await imageCompression(img, option);
+    return compressedImg;
+  };
+
+  const check = async () => {
     const files = inputRef.current?.files;
     const dialog = document.getElementById("modal-dialog");
     dialog?.setAttribute("open", "");
     const reader = new FileReader();
     if (files && files[0]) {
-      setFileList(files);
-      reader.readAsDataURL(files[0]);
-      reader.onloadend = () => {
-        if (reader.result) setTmpImg(reader.result as string);
-      };
+      try {
+        const compressedImg = await compressImg(files[0]);
+        setUploadedImg(compressedImg);
+        reader.readAsDataURL(compressedImg);
+        reader.onloadend = () => {
+          if (reader.result) setTmpImg(reader.result as string);
+        };
+      } catch (err) {
+        alert("다시 시도해주세요.");
+      }
     }
     document.body.onfocus = null;
   };
@@ -59,8 +75,8 @@ function AvatarUploadModal(props: modalProps) {
   const uploadAvatarHandler = (e: React.MouseEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
-    if (fileList && fileList[0]) {
-      formData.append("avatar", fileList[0]);
+    if (uploadedImg) {
+      formData.append("avatar", uploadedImg);
       avatarMutation.mutate(formData);
     } else {
       setNoti("이미지를 선택해주세요.");
@@ -74,7 +90,9 @@ function AvatarUploadModal(props: modalProps) {
         <span> png / 20KB 이하 업로드 가능</span>
         <S.SelectImgWrapper>
           <S.Img src={tmpImg ? tmpImg : props.prevUrl} alt="프로필 이미지" />
-          <S.Label htmlFor="avatar">{tmpImg? "다른 이미지 선택" : "프로필 이미지 선택"}</S.Label>
+          <S.Label htmlFor="avatar">
+            {tmpImg ? "다른 이미지 선택" : "프로필 이미지 선택"}
+          </S.Label>
           <input
             style={{ display: "none" }}
             ref={inputRef}
